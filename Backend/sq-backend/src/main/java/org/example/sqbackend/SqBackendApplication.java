55304b9c40c00f794d525b8299089bf0a015211e
Victor BARDIN <bardinvictor702@gmail.com>
2024-03-22 10:05:37 +0100

    Adding backend init

---
+++ Backend/sq-backend/src/main/java/org/example/sqbackend/SqBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.sqbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/sq-backend/src/test/java/org/example/sqbackend/SqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sqbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
