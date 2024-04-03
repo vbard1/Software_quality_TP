0987450150517b2fea28bc3dc1e349106a4e72be
Victor BARDIN <bardinvictor702@gmail.com>
2024-04-03 11:20:21 +0200

    Creating test class and empty initializer

---
+++ Backend/sq-backend/src/main/java/org/example/sqbackend/initialization/DataInitializer.java
@@ -0,0 +1,10 @@
+package org.example.sqbackend.initialization;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer {
+    public void initializeData() {
+        // Cette méthode sera implémentée plus tard
+    }
+}
