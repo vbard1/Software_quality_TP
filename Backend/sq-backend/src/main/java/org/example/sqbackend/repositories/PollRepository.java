7847b9e71cbcaa12f05fbea27581e5550cebce1e
Victor BARDIN <bardinvictor702@gmail.com>
2024-04-19 18:27:57 +0200

    Adding generic classes

--- Backend/sq-backend/src/main/java/org/example/sqbackend/repositories/PollRepository.java
+++ Backend/sq-backend/src/main/java/org/example/sqbackend/repositories/PollRepository.java
@@ -4,6 +4,8 @@ import org.example.sqbackend.models.Poll;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PollRepository extends JpaRepository<Poll, Integer> {
 }
